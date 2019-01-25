8b1503479e919863486e6a5381e3158ded515249
Pizzaaboi <Hwieringa5@gmail.com>
2019-01-25 12:34:16 +0100

    changed code and added pedestiran crossing
    pedestrian crossing is not working yet, I'll make a issue about it

--- Basis_Opdracht/Basis_Opdracht.ino
+++ Basis_Opdracht/Basis_Opdracht.ino
@@ -5,17 +5,23 @@ pinMode(1, OUTPUT);
 pinMode(2, OUTPUT);
 pinMode(3, OUTPUT);
 pinMode(4, OUTPUT);
+pinMode(5, OUTPUT);
 pinMode(8, OUTPUT);
 pinMode(9, OUTPUT);
 pinMode(10, OUTPUT);
 pinMode(11, OUTPUT);
+pinMode(12, OUTPUT);
+pinMode(13, INPUT); 
 }
 
 void loop() {
-  digitalWrite(1, HIGH);
+  
+if(digitalRead(13) == HIGH) { 
   digitalWrite(2, HIGH);
   digitalWrite(3, HIGH);
   digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(12, LOW);
   delay(500);
   digitalWrite(1, LOW);
   digitalWrite(8, HIGH);
@@ -43,4 +49,46 @@ void loop() {
   delay(1000);
   digitalWrite(11, LOW); 
   digitalWrite(4, HIGH);
+  delay(500);
+  digitalWrite(5, LOW);
+  digitalWrite(12, HIGH);
+  delay(1000);
 }
+else {
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+  delay(500);
+  digitalWrite(1, LOW);
+  digitalWrite(8, HIGH);
+  delay(1000); 
+  digitalWrite(8, LOW);
+  digitalWrite(1, HIGH);
+  digitalWrite(2, HIGH); 
+  delay(500); 
+  digitalWrite(2, LOW);
+  digitalWrite(9, HIGH);
+  delay(1000); 
+  digitalWrite(9, LOW); 
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  delay(500); 
+  digitalWrite(3, LOW); 
+  digitalWrite(10, HIGH);
+  delay(1000);
+  digitalWrite(4, HIGH); 
+  digitalWrite(3, HIGH);
+  digitalWrite(10, LOW);
+  delay(500);
+  digitalWrite(4, LOW);
+  digitalWrite(11, HIGH);
+  delay(1000);
+  digitalWrite(11, LOW); 
+  digitalWrite(4, HIGH);
+  delay(1000);
+}
+}
+
+// lampje voetganger pin7 en knopje pin13
+ 
