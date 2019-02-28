29f588fc84232a5609f2dbef02e0f6e1e90c1a80
Pizzaaboi <Hwieringa5@gmail.com>
2019-02-28 16:04:07 +0100

    only pedestrian
    now only the pedestrian, tested it and it still works

--- Voetgangers_Opdracht/Voetgangers_Opdracht.ino
+++ Voetgangers_Opdracht/Voetgangers_Opdracht.ino
@@ -1,9 +1,83 @@
-void setup() 
-{
-  // put your setup code here, to run once:
+ int remember = 0;
+
+void setup() {
+int RoadA = 6;
+int RoadB = 7; 
+int RoadC = 14;
+int RoadD = 15;
+pinMode(1, OUTPUT);
+pinMode(2, OUTPUT);
+pinMode(3, OUTPUT);
+pinMode(4, OUTPUT);
+pinMode(5, OUTPUT);
+pinMode(8, OUTPUT);
+pinMode(9, OUTPUT);
+pinMode(10, OUTPUT);
+pinMode(11, OUTPUT);
+pinMode(12, OUTPUT);
+pinMode(13, INPUT); 
+Serial.begin(9600);
 }
 
-void loop() 
+void loop() {
+
+digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(12, LOW); 
+  wait(500);
+  digitalWrite(1, LOW);
+  digitalWrite(8, HIGH);
+  wait(1000); 
+  digitalWrite(8, LOW);
+  digitalWrite(1, HIGH);
+  digitalWrite(2, HIGH); 
+  wait(500); 
+  digitalWrite(2, LOW);
+  digitalWrite(9, HIGH);
+  wait(1000); 
+  digitalWrite(9, LOW); 
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  wait(500); 
+  digitalWrite(3, LOW); 
+  digitalWrite(10, HIGH);
+  wait(1000);
+  digitalWrite(4, HIGH); 
+  digitalWrite(3, HIGH);
+  digitalWrite(10, LOW);
+  wait(500);
+  digitalWrite(4, LOW);
+  digitalWrite(11, HIGH);
+  wait(1000);
+  digitalWrite(11, LOW); 
+  digitalWrite(4, HIGH);
+ 
+
+if(remember == 1)
 {
-  // put your main code here, to run repeatedly:
+  wait(500);
+  digitalWrite(12, HIGH); 
+  digitalWrite(5, LOW); 
+  wait(1000);
+  digitalWrite(12, LOW);
+  digitalWrite(5, HIGH);
+  remember = 0;
 }
+}
+
+void wait(int seconds)
+{
+ while(seconds > 0)
+  {
+    if(digitalRead(13)==HIGH)
+    {
+      remember = 1;
+      Serial.println("knopje aan");
+    }
+    delay(1);
+    seconds--;
+  }
+}
+ 
