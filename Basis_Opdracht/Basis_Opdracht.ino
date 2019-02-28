b55a2e62fa60de31e8f911b0ed6593ec3e731b39
Pizzaaboi <Hwieringa5@gmail.com>
2019-02-28 14:16:58 +0100

    Basis for Busline
    Added Input lines for potential buttons (don't know if any more can fit)
    and added what I think is a good idea to do the busline

--- Basis_Opdracht/Basis_Opdracht.ino
+++ Basis_Opdracht/Basis_Opdracht.ino
@@ -1,4 +1,4 @@
-int remember = 0;
+ int remember = 0;
 
 void setup() {
 pinMode(1, OUTPUT);
@@ -6,12 +6,16 @@ pinMode(2, OUTPUT);
 pinMode(3, OUTPUT);
 pinMode(4, OUTPUT);
 pinMode(5, OUTPUT);
+pinMode(6, INPUT); //ROAD 1
+pinMode(7, INPUT); //ROAD 2
 pinMode(8, OUTPUT);
 pinMode(9, OUTPUT);
 pinMode(10, OUTPUT);
 pinMode(11, OUTPUT);
 pinMode(12, OUTPUT);
 pinMode(13, INPUT); 
+pinMode(14, INPUT); //ROAD 3
+pinMode(15, INPUT); //ROAD 4
 Serial.begin(9600);
 }
 
@@ -75,5 +79,38 @@ void wait(int seconds)
     delay(1);
     seconds--;
   }
+  while(seconds > 0)
+  { if digitalRead(Road1)==HIGH)
+    {
+      RoadA = 1
+      Serial.println("Road A")
+     
+    }
+  }
+  while(seconds > 0)
+  { if digitalRead(Road2)==HIGH)
+    {
+      RoadA = 1
+      Serial.println("Road B")
+    }
+  }
+    while(seconds > 0)
+  { if digitalRead(Road3)==HIGH)
+    {
+      RoadA = 1
+      Serial.println("Road C")
+    }
+  }
+    while(seconds > 0)
+  { if digitalRead(Road4)==HIGH)
+    {
+      RoadA = 1
+      Serial.println("Road D")
+    }
+  }
 }
+
+
+
+
  
